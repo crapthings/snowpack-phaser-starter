3e85478776702e7c0e71bafe50eb14146e2cfcfa
crapthings <crapthings@gmail.com>
2022-01-15 14:49:15 +0800

    add base scene

---
+++ src/scenes/base.js
@@ -0,0 +1,23 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Scene {
+  constructor () {
+    super('base')
+  }
+
+  init () {
+
+  }
+
+  preload () {
+
+  }
+
+  create () {
+
+  }
+
+  update () {
+
+  }
+}
